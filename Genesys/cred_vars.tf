f9132fd71cd70390d7e05ec59b54d779493630ef
tamilalaghan <tamil.accenture@gmail.com>
2024-05-24 07:54:35 -0400

    structure changes

--- Genesys/cred_vars.tf
+++ Genesys/cred_vars.tf
@@ -1,10 +1,10 @@
 variable "client-id" {
-    default = "c47d0c25-e784-4342-8a8a-9c13878e4eae"
+    default = ""
     
 }
 
 variable "client-secret" {
-    default = "oUUilhWIlNDlKlZTGL751DRbcaC1IHZYO8G0lHFZ6XU"
+    default = ""
     
 }
 
